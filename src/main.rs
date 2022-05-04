87da7436120b736462be48eb73f84b8208f8b049
Aamir <djzaamir@gmail.com>
2022-05-04 17:16:49 +0500

    Getting input from the user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+//For input and output operations
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+
+    println!("\n\t\tGuess the number\n");
+
+    println!("Please Guess A number : ");
+
+    let mut guess : String = String::new();
+
+    /*
+    1) stdin allows to get the handle on the default input stream, and its coming from the terminal
+    2) read_line will read the input from the terminal until `\n` and will place into the provided reference of mutable variable
+    3) Expect will be called with the provided error message incase of any problems
+    */
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Error reading the input.");
 }
